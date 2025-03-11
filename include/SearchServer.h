d11af45eac1766ec7b57ebc4afd1fc7e23272484
Савелий <saveliikulakov@mail.ru>
2025-03-11 14:35:02 +0500

    Add files via upload

---
+++ include/SearchServer.h
@@ -0,0 +1,25 @@
+#ifndef SEARHSERVER_H
+#define SEARHSERVER_H
+#include "InvertedIndex.h"
+using namespace std;
+
+struct RelativeIndex {
+  size_t doc_id;
+  float rank;
+
+  bool operator == (const RelativeIndex& other) const {
+    return (doc_id == other.doc_id && rank == other.rank);
+  }
+};
+
+class SearchServer {
+  public:
+    SearchServer(InvertedIndex& idx) : _index(idx) {};
+
+    vector<vector<RelativeIndex>> search(const vector<string>& queries_input);
+    private:
+      InvertedIndex& _index;
+
+};
+
+#endif //SEARHSERVER_H
