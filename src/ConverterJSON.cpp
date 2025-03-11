cb402758c4abb0d594c903dc91d4eade96803308
Савелий <saveliikulakov@mail.ru>
2025-03-11 14:38:26 +0500

    Add files via upload

---
+++ src/ConverterJSON.cpp
@@ -0,0 +1,140 @@
+#include "ConverterJSON.h"
+#include <iostream>
+#include <fstream>
+#include "nlohmann/json.hpp"
+#include <exception>
+#include "Exceptions.h"
+#include "Checks.h"
+using namespace std;
+
+ConverterJSON::ConverterJSON(){};
+ConverterJSON::~ConverterJSON(){};
+
+vector<string> ConverterJSON::getFiles () {
+  cout << "Loading config file..." << std::endl;
+  ifstream configFile("../data/config.json");
+  if(checkingFilesIsOpen(configFile)) {
+    if(configFile.is_open()) {
+      try{
+        nlohmann::json config;
+        configFile >> config;
+        configFile.close();
+        if(config["config"].is_null()) {throw NoSuchConfig();}
+        cout << "Loaded config file." << endl;
+        cout << config["name"] << endl;
+        cout << config["version"] << endl;
+        configData = config;
+      }catch (NoSuchConfig& e) {
+        cerr << e.what() << endl;
+      }
+    }
+  }
+  vector<string> path;
+  vector<string> files;
+  try {
+    if(configData.contains("files") && configData["files"].is_array()) {
+      for(auto& file : configData["files"]) {
+        if(file.is_string()) {
+          path.push_back(file);
+
+        }else {
+          cout << "File not string" << endl;
+        }
+      }
+    }else {
+      throw NoSuchFile();
+    }
+  }catch(NoSuchFile& e) {
+    cerr << e.what() << endl;
+  }
+
+  ifstream file;
+  for(auto& _path : path) {
+    file.open(_path);
+    if(!file.is_open()) {
+      cerr << "File not open!" << endl;
+    }else {
+      stringstream buffer;
+      buffer << file.rdbuf();
+      if(checkingFiles(buffer)) {
+        string content = buffer.str();
+        files.push_back(content);
+      }
+    }
+    file.close();
+  }
+  return files;
+}
+
+int ConverterJSON::GetResponsesLimit () {
+  if(configData.contains("responseLimit")) {
+    return configData["max_responses"].get<int>();
+  } else {
+    return 5;
+  }
+}
+
+vector<string> ConverterJSON::GetRequests() {
+  vector<string> requests;
+  ifstream fileRequests("../data/requests.json");
+  checkingFilesIsOpen(fileRequests);
+  nlohmann::json _requests;
+  fileRequests >> _requests;
+  fileRequests.close();
+  try {
+    if(_requests.contains("requests") && _requests["requests"].is_array()) {
+      if(checkingRequests(_requests)) {
+        for(auto& request : _requests["requests"]) {
+          requests.push_back(request);
+        }
+      }
+      return requests;
+    }else {
+      throw NoSuchRequests();
+    }
+  }catch(NoSuchRequests& e) {
+    cerr << e.what() << endl;
+    return requests;
+  }
+}
+
+void ConverterJSON::putAnswers(std::vector<std::vector<std::pair<int, float>>>answers) {
+  nlohmann::json answersJson;
+  nlohmann::json answersObject;
+  int requestIndex = 1;
+  for(auto& answer : answers) {
+    string requestID = "request" + to_string(requestIndex);
+    nlohmann::json requestObject;
+
+    if(answer.empty()) {
+      requestObject["relevance"] = {};
+      requestObject["result"] = "false";
+    }else {
+      nlohmann::json relevanceArray;
+      for(auto& result : answer) {
+        nlohmann::json resultObject;
+        resultObject["docid"] = result.first;
+        resultObject["rank"] = result.second;
+        relevanceArray.push_back(resultObject);
+      }
+      requestObject["relevance"] = relevanceArray;
+      requestObject["result"] = "true";
+    }
+    answersObject[requestID] = requestObject;
+    requestIndex++;
+  }
+  answersJson["answers"] = answersObject;
+
+  std::ofstream outputFile("../data/answers.json");
+  outputFile << answersJson.dump(4);
+  outputFile.close();
+}
+
+
+
+
+
+
+
+
+
---
+++ src/SearchServer.cpp
@@ -0,0 +1,115 @@
+#include "SearchServer.h"
+
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include "Checks.h"
+#include "InvertedIndex.h"
+#include <set>
+#include <vector>
+#include "ConverterJSON.h"
+
+vector<vector<RelativeIndex>> SearchServer::search(const vector<string>& queries_input) {
+    vector<vector<RelativeIndex>> results;
+    ConverterJSON convert;
+    vector<vector<std::pair<int, float>>>answers;
+
+    for (auto& query : queries_input) {
+        stringstream ss(query);
+        string word;
+        vector<string> query_words;
+        unordered_set<string> unique_query_words;
+        while(ss >> word) {
+            transform(word.begin(), word.end(), word.begin(), ::tolower);
+            query_words.push_back(word);
+        }
+        for(auto& word1 : query_words) {
+            unique_query_words.insert(word1);
+        }
+        vector<string> sorted_query = sorting_queries(unique_query_words,_index);
+        set<size_t> doc_ids;
+        for(auto& word3 : sorted_query) {
+            for(auto& entry : _index.GetWordCount(word3)) {
+                doc_ids.emplace(entry.doc_id);
+            }
+        }
+        vector<Entry> entries;
+        bool checkingWords = false;
+        for(auto& entry : _index.GetWordCount(sorted_query[0])) {
+            entries.push_back(entry);
+        }
+        vector<Entry> entries_copy = entries;
+        for(auto& word3 : sorted_query) {
+            if(!entries_copy.empty()) {
+                for(auto& entry : entries_copy) {
+                    bool found = false;
+                    for(auto& entry2 : _index.GetWordCount(word3)) {
+                        if(entry.doc_id == entry2.doc_id) {
+                            found = true;
+                        }
+                    }
+                    if(!found) {erase(entries_copy, entry);}
+                }
+            }
+        }
+        if(!entries_copy.empty()) {
+            vector<RelativeIndex> relative_indices;
+            map<size_t, size_t> doc_id_absolut_rel;
+            for(auto& id : doc_ids) {
+                size_t absolute_relevance = 0;
+                for(auto& query_word : sorted_query) {
+                    for(auto& entry2 : _index.GetWordCount(query_word)) {
+                        if(id == entry2.doc_id) {
+                            absolute_relevance += entry2.count;
+                        }
+                    }
+                }
+                doc_id_absolut_rel[id] = absolute_relevance;
+            }
+
+            size_t max_absolute_rel = 0;
+            for(auto& [doc_id, absolute_relevance] : doc_id_absolut_rel) {
+                max_absolute_rel = max(max_absolute_rel, absolute_relevance);
+            }
+            for(auto& [doc_id, absolute_relevance] : doc_id_absolut_rel) {
+                float relative_relevance;
+                if(max_absolute_rel > 0) {
+                    relative_relevance = (float)absolute_relevance / (float)max_absolute_rel;
+                }
+                else {
+                    relative_relevance = 0.0f;
+                }
+
+
+                relative_indices.push_back({doc_id, relative_relevance});
+            }
+
+            sort(relative_indices.begin(), relative_indices.end(), [](const RelativeIndex& a, const RelativeIndex& b) {
+                if(a.rank > b.rank) {
+                    return a.rank > b.rank;
+                }else if(a.rank < b.rank) {
+                    return a.rank > b.rank;
+                }else if(a.rank == b.rank) {
+                    return a.doc_id < b.doc_id;
+                }
+            });
+            int count = convert.GetResponsesLimit();
+            while(relative_indices.size() > count) {
+                relative_indices.pop_back();
+            }
+            results.push_back(relative_indices);
+            vector<pair<int, float>> answer_for_query;
+            for (const auto& rel_index : relative_indices) {
+                answer_for_query.emplace_back(rel_index.doc_id, rel_index.rank);
+            }
+            answers.push_back(answer_for_query);
+        }
+        else {
+            answers.push_back({});
+            results.push_back({});
+        }
+    }
+
+    convert.putAnswers(answers);
+    return results;
+}
